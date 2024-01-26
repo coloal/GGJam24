349153b867dc059531b2fd6122750f323d8923b2
samigozz <149153729+OmarMG14@users.noreply.github.com>
2024-01-26 23:22:42 +0100

    Revert "Merge branch 'develop' of https://github.com/coloal/GGJam24 into develop"
    This reverts commit 2046484be13841c06856aec97ba47e447e05ae98, reversing
    changes made to e28f94133880f9300a536640f8aa6dcf85df2cfc.

--- Assets/Scripts/UI/MainMenuScene/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuScene/MainMenuController.cs
@@ -4,6 +4,6 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public void GoToMainGame() {
-        SceneManager.LoadScene(ScenesNames.MainGameScene);
+        
     }
 }
