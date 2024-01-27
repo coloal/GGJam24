4cfd21a723aeeeee3b229e925c04a0f6e5e37aac
Adrihc13 <adr13suki@gmail.com>
2024-01-27 22:37:22 +0100

    Updated hitmen OnClickEvents

--- Assets/Input/InputController.cs
+++ Assets/Input/InputController.cs
@@ -12,16 +12,23 @@ public class InputController : MonoBehaviour
     public void OnLeftClickRay() 
     {
 
-        RaycastHit hit;
-        if (Physics.Raycast(gameCamera.ScreenPointToRay(MousePosition), out hit)) {
+        //RaycastHit hit;
+        //if (Physics.Raycast(gameCamera.ScreenPointToRay(MousePosition), out hit))
+        Ray ray = gameCamera.ScreenPointToRay(MousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        
+        hit.collider.GetComponent<IClickable>()?.OnClick();
+
+        /*if (Physics2D.Raycast(ray.origin, ray.direction)) 
+        {
             hit.collider.GetComponent<IClickable>()?.OnClick();
-        }
+        }*/
     }
 
     public void OnMouseOverRay(InputValue value) 
     {
         MousePosition = value.Get<Vector2>();
-        Debug.Log(""+MousePosition);
+        //Debug.Log(""+MousePosition);
         RaycastHit hit;
         if (Physics.Raycast(gameCamera.ScreenPointToRay(MousePosition), out hit)) {
             hit.collider.GetComponent<IClickable>()?.OnMouseOver();
