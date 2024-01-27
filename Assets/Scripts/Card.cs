0a77a96693be0e33e8970ecad9f133bc764d1b1b
Michael Woo <302maik@gmail.com>
2024-01-27 20:43:55 +0100

    Refactored CardTemplate data assets

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -26,45 +26,13 @@ public class Card : MonoBehaviour
 
     //Information of the card
     private string NameOfCard;
-    private string Name;
-    private string Gen;
-    private string Background;
-
-    //Indice del chat a reproducir se debe autoincrementar al terminar con esta carta
-    private int indexChats = 0;
-    private List<string> Chats;
-
-    private List<string> LeftChoices;
-    private List<string> RightChoices;
-
-    //next cards to appear
-    private List<GameObject> LeftCards;
-    private List<GameObject> RightCards;
 
     //Incrementos/Decrementos
-    public int InfluenceStat; //Nombre provisional pdt
-    public int MoneyStat; //Nombre provisional pdt
-    public int ViolenceStat; //Nombre provisional pdt
-
-    //Options text
-    private string LeftText;
-    private string RightText;
+    public int InfluenceStat;
+    public int MoneyStat;
+    public int ViolenceStat;
 
     public List<HitManTypes> ListHitmanTypes;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ShowText(bool IsLeft)
     {
@@ -83,18 +51,6 @@ public class Card : MonoBehaviour
     {
         //Set informaci�n del DataCard
         NameOfCard = DataCard.NameOfCard;
-        Name = DataCard.Name;
-        Gen = DataCard.Gen;
-        Background = DataCard.Background;
-
-        indexChats = DataCard.indexChats;
-        Chats = DataCard.Chats;
-
-        LeftChoices = DataCard.LeftChoices;
-        RightChoices = DataCard.RightChoices;
-
-        LeftCards = DataCard.LeftCards;
-        RightCards = DataCard.RightCards;
 
         InfluenceStat = DataCard.InfluenceStat;
         MoneyStat = DataCard.MoneyStat;
--- Assets/Scripts/Data/ScriptableObjects/CardTemplate.cs
+++ Assets/Scripts/Data/ScriptableObjects/CardTemplate.cs
@@ -7,21 +7,7 @@ public class CardTemplate : ScriptableObject
 {
     //Information of the card
     public string NameOfCard;
-    public string Name;
-    public string Gen;
-    public int Age;
     public string Background;
-
-    //Indice del chat a reproducir se debe autoincrementar al terminar con esta carta
-    public int indexChats = 0;
-    public List <string> Chats;
-    
-    public List<string> LeftChoices;
-    public List<string> RightChoices;
-
-    //next cards to appear
-    public List<GameObject> LeftCards;
-    public List<GameObject> RightCards;
 
     //Incrementos/Decrementos
     public int ViolenceStat; //Nombre provisional pdt
