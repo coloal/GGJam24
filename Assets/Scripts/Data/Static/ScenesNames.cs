e2ff969a1310bae999484299fb39b03c297b3f2e
Luchi <lucialopmig7@gmail.com>
2024-01-28 02:40:06 +0100

    Menu Principal y Creditos
    Ya funcionan los menuss!!

--- Assets/Scripts/Data/Static/ScenesNames.cs
+++ Assets/Scripts/Data/Static/ScenesNames.cs
@@ -1,4 +1,5 @@
 public static class ScenesNames {
     public const string MainMenuScene = "MainMenu";
     public const string MainGameScene = "MainGame";
+    public const string CreditsScene = "Credits";
 }
--- Assets/Scripts/UI/MainMenuScene/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuScene/MainMenuController.cs
@@ -6,4 +6,10 @@ public class MainMenuController : MonoBehaviour
     public void GoToMainGame() {
         SceneManager.LoadScene(ScenesNames.MainGameScene);
     }
+     public void GoToCredits() {
+        SceneManager.LoadScene(ScenesNames.CreditsScene);
+    }
+    public void GoToMainMenu() {
+        SceneManager.LoadScene(ScenesNames.MainMenuScene);
+    }
 }
