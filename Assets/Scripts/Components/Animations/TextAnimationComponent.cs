82a93a8de507165620157ce869102d2d3982d84b
Michael Woo <302maik@gmail.com>
2024-03-30 12:14:23 +0100

    Fixed typewriter animation bug that added garbage data to the text typed by the animation

--- Assets/Scripts/Components/Animations/TextAnimationComponent.cs
+++ Assets/Scripts/Components/Animations/TextAnimationComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -9,17 +10,42 @@ public class TextAnimationComponent : MonoBehaviour
     [Header("Animation settings")]
     [SerializeField] private float perCharTimeDelay = 0.05f;
 
+    private Dictionary<int, IEnumerator> activeAnimations;
+
+    void Awake()
+    {
+        activeAnimations = new Dictionary<int, IEnumerator>();
+    }
+
     public void PlayTypewriterAnimation(TextMeshProUGUI textMesh, string text)
     {
-        float animationDelay = perCharTimeDelay;
+        IEnumerator activeAnimationCoroutine;
+        activeAnimations.TryGetValue(textMesh.GetInstanceID(), out activeAnimationCoroutine);
+        if (activeAnimationCoroutine != null)
+        {
+            StopCoroutine(activeAnimationCoroutine);
+            activeAnimations.Remove(textMesh.GetInstanceID());
+        }
+
+        IEnumerator typewriterAnimationCoroutine = 
+            TypewriterAnimation(textMesh, text, perCharTimeDelay,
+                onAnimationEnded: () => {
+                    activeAnimations.Remove(textMesh.GetInstanceID());
+                });
+        activeAnimations.Add(textMesh.GetInstanceID(), typewriterAnimationCoroutine);
+        
+        StartCoroutine(typewriterAnimationCoroutine);
+    }
+
+    private IEnumerator TypewriterAnimation(TextMeshProUGUI textMesh, 
+        string text, float delaySeconds, Action onAnimationEnded)
+    {
         textMesh.text = "";
         foreach (char letter in text)
         {
-            GameUtils.CreateTemporizer(() =>
-            {
-                textMesh.text += letter;
-            }, animationDelay, this);
-            animationDelay += perCharTimeDelay;
+            yield return new WaitForSeconds(delaySeconds);
+            textMesh.text += letter;
         }
+        onAnimationEnded();
     }
 }
