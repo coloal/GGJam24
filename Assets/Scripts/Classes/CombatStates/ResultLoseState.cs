456125c31151fd7f57249de3d32ab142d6cc568d
Michael Woo <302maik@gmail.com>
2024-07-02 11:32:48 +0200

    Added animation feedbacks for ResultLoseState

--- Assets/Scripts/Classes/CombatStates/ResultLoseState.cs
+++ Assets/Scripts/Classes/CombatStates/ResultLoseState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class ResultLoseState : CombatState
@@ -17,8 +18,36 @@ public class ResultLoseState : CombatState
     {
     }
 
-    public override void ProcessImplementation(CombatManager.CombatContext combatContext)
+    public override async void ProcessImplementation(CombatManager.CombatContext combatContext)
     {
+        // This method should never do something, as the combat design implies
+        await ReturnPlayerCardsFromHandToDeck(combatContext);
+        
         PostProcess(combatContext);
+    }
+
+    async Task ReturnPlayerCardsFromHandToDeck(CombatManager.CombatContext combatContext)
+    {
+        List<Transform> cardInHandContainers = combatContext.GetPlayerCardInHandContainers();
+        for (int i = cardInHandContainers.Count - 1; i >= 0; i--)
+        {
+            if (cardInHandContainers[i].childCount > 0)
+            {
+                CombatCard combatCard = cardInHandContainers[i].GetChild(0).GetComponent<CombatCard>();
+                if (combatCard != null)
+                {
+                    combatCard.transform.SetParent(
+                        combatContext.playerDeck.transform.parent,
+                        worldPositionStays: true
+                    );
+                    await CombatSceneManager.Instance.ProvideCombatFeedbacksManager()
+                        .PlayReturnCardToDeck(
+                            cardToReturn: combatCard,
+                            combatContext.playerDeck.transform
+                        );
+                    combatCard.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
