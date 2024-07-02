cddb0ff1fe3be4de987e6048dc80aea72b974146
Luchi <lucialopmig7@gmail.com>
2024-07-02 19:48:55 +0200

    Navigation scenes and tuto
    Added navigation to main menu, added credits scene and test for tutorial with notebook

--- Assets/Scripts/Managers/SceneManagers/MainMenuManager.cs
+++ Assets/Scripts/Managers/SceneManagers/MainMenuManager.cs
@@ -6,4 +6,7 @@ public class MainMenuManager : MonoBehaviour
     public void GoToMainGame() {
         SceneManager.LoadScene(ScenesNames.MainGameScene);
     }
+    public void GoToCreditsMenu() {
+        SceneManager.LoadScene(ScenesNames.CreditsMenuScene);
+    }
 }
