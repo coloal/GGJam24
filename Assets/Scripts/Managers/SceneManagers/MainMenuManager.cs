a4f3cd0c2e6650846d2d70fa16443039e425e173
Michael Woo <302maik@gmail.com>
2024-07-06 17:09:26 +0200

    Refactored MainMenu scene by deleting unused game managers

--- Assets/Scripts/Managers/SceneManagers/MainMenuManager.cs
+++ Assets/Scripts/Managers/SceneManagers/MainMenuManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour
+public class MainMenuManager : BaseSceneManager
 {
     public void GoToMainGame() {
         SceneManager.LoadScene(ScenesNames.MainGameScene);
